627beb858d927358844f04ab3061677d05ae7364
IZFAISSAL <faissal.izermine@eleves.enpc.fr>
2025-11-26 12:33:02 +0100

    indentation

--- main.py
+++ main.py
@@ -42,7 +42,8 @@ def parse_args_and_config():
         new_config = dict2namespace(config)
     else:
         with open(os.path.join(args.log, 'config.yml'), 'r') as f:
-        config = yaml.load(f, Loader=yaml.UnsafeLoader)
+            config = yaml.load(f, Loader=yaml.UnsafeLoader)
+            
         if isinstance(config, dict):
             new_config = dict2namespace(config)
         else:

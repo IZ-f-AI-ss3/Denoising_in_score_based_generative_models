9de2876a0a330ee0f01fcd589fefe6f8adf03d70
IZFAISSAL <faissal.izermine@eleves.enpc.fr>
2025-11-29 17:25:13 +0100

    half_denoising

--- runners/anneal_runner.py
+++ runners/anneal_runner.py
@@ -305,8 +305,8 @@ class AnnealRunner():
             samples = torch.rand(grid_size ** 2, 3, 32, 32, device=self.config.device)
 
             # all_samples = self.anneal_Langevin_dynamics(samples, score, sigmas, 100, 0.00002)
+            
             all_samples = self.half_denoising_anneal_Langevin_dynamics(samples, score, sigmas, 100, 0.00002)
-
 
             for i, sample in enumerate(tqdm.tqdm(all_samples, total=len(all_samples), desc='saving images')):
                 sample = sample.view(grid_size ** 2, self.config.data.channels, self.config.data.image_size,
